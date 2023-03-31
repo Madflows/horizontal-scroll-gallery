7e37a01aad979f48a400d87ef2e9a94cc8579473
Folarin Lawal <lawalfolarin6@gmail.com>
2023-03-31 23:34:51 +0100

    added tailwindcss for styling

--- pages/_app.js
+++ pages/_app.js
@@ -5,14 +5,13 @@ import Loader from '@/components/Loader';
 
 export default function App({ Component, pageProps }) {
   return (
-    <div className='relative'>
-      <div className='invisible lg:visible'>
+    <div className='relative max-sm:overflow-hidden max-sm:h-screen max-sm:w-screen'>
+      <div className='invisible  lg:visible'>
         <Loader />
         <Component {...pageProps} />
       </div>
       <div className='flex lg:hidden absolute inset-0 z-[10] bg-[#191919] text-[#ffffff] text-lg items-center justify-center font-jamjuree'>
         <div className='flex flex-col gap-4 items-center'>
-          
           <p className='text-center font-clash text-sm font-semibold w-[80vw] max-w-sm'>
             THIS SITE CAN ONLY BE VIEWED ON A DESKTOP DEVICE.
           </p>
